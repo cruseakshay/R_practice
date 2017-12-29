fa9bee8f6ff148d54c1c1fc90a461a9b81fd469c
Akshaykumar <cruseakshay@gmail.com>
2017-12-29 10:25:34 +0530

    added preProcess for median impute

--- Preprocessing data in R.R
+++ Preprocessing data in R.R
@@ -1,1 +1,14 @@
 # dataset : Wisconsin Breast Cancer dataset
+# This dataset presents a classic binary classification problem: 
+# 50% of the samples are benign, 50% are malignant, and the challenge is to identify which are which.
+
+# Apply median imputation: model
+model <- train(
+  x = breast_cancer_x, y = breast_cancer_y,
+  method = "glm",
+  trControl = myControl,
+  preProcess = "medianImpute"
+)
+
+# Print model to console
+model
