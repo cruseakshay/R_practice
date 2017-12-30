437b999cb8fe74dc167506fcc6bbe158eeeef8ee
Akshaykumar <cruseakshay@gmail.com>
2017-12-30 13:52:33 +0530

    added remaining preprocessing techniques

--- Preprocessing data in R.R
+++ Preprocessing data in R.R
@@ -26,3 +26,56 @@ model2
 
 # compare plot result for out of box samples
 dotplot(resamples, metric = "ROC")
+
+# Fit glm with median imputation: model1
+model1 <- train(
+  x = breast_cancer_x, y = breast_cancer_y,
+  method = "glm",
+  trControl = myControl,
+  preProcess = "medianImpute"
+)
+
+# Print model1
+model1
+
+# Combining preprocessing methods
+# Fit glm with median imputation and standardization: model2
+model2 <- train(
+  x = breast_cancer_x, y = breast_cancer_y,
+  method = "glm",
+  trControl = myControl,
+  preProcess = c("medianImpute", "center", "scale")
+)
+
+# Print model2
+model2
+
+# Remove near zero variance predictors
+# dataset : blood-brain dataset.
+# Identify near zero variance predictors: remove_cols
+remove_cols <- nearZeroVar(bloodbrain_x, names = TRUE, freqCut = 2, uniqueCut = 20)
+
+# Get all column names from bloodbrain_x: all_cols
+all_cols <- names(bloodbrain_x)
+
+# Remove from data: bloodbrain_x_small
+bloodbrain_x_small <- bloodbrain_x[ , setdiff(all_cols, remove_cols)]
+
+# note: use "zv" and "nzv" in preProcess for removing constant value predictor
+
+# Fit model on reduced blood-brain data
+# Fit model on reduced data: model
+model <- train(x = bloodbrain_x_small, y = bloodbrain_y, method = "glm")
+
+# Print model to console
+model
+
+# Using PCA as an alternative to nearZeroVar()
+# Fit glm model using PCA: model
+model <- train(
+  x = bloodbrain_x, y = bloodbrain_y,
+  method = "glm", preProcess = "pca"
+)
+
+# Print model to console
+model
