ada4c6e50b02fd02bf5a4e68ab087d947a06a096
Akshaykumar <cruseakshay@gmail.com>
2017-12-30 15:35:30 +0530

    added intro to k-means

---
+++ Unsupervised Learning in R/Unsupervised learning.R
@@ -0,0 +1,90 @@
+# k-means clustering
+# Create the k-means model: km.out
+km.out <- kmeans(x, center = 3, nstart = 20)
+
+# Inspect the result
+summary(km.out)
+
+# Results of kmeans()
+# Print the cluster membership component of the model
+km.out$cluster
+
+# Print the km.out object
+print(km.out)
+
+# Visualizing and interpreting results of kmeans()
+# Scatter plot of x
+plot(x, col = km.out$cluster, main = "k-means with 3 clusters", xlab = "", ylab = "")
+
+# Handling random algorithms
+# Set up 2 x 3 plotting grid
+par(mfrow = c(2, 3))
+
+# Set seed
+set.seed(1)
+
+for(i in 1:6) {
+  # Run kmeans() on x with three clusters and one start
+  km.out <- kmeans(x, center = 3, nstart = 1)
+  
+  # Plot clusters
+  plot(x, col = km.out$cluster, 
+       main = km.out$tot.withinss, 
+       xlab = "", ylab = "")
+}
+
+# Selecting number of clusters
+# Plots displaying information help to determine the number of clusters and are often referred to as scree plots.
+
+# note : The ideal plot will have an elbow where the quality measure improves more slowly as the number of clusters increases. 
+# This indicates that the quality of the model is no longer improving substantially as the model complexity (i.e. number of clusters) increases.
+
+# Initialize total within sum of squares error: wss
+wss <- 0
+
+# For 1 to 15 cluster centers
+for (i in 1:15) {
+  km.out <- kmeans(x, centers = i, nstart = 20)
+  # Save total within sum of squares to wss variable
+  wss[i] <- km.out$tot.withinss
+}
+
+# Plot total within sum of squares vs. number of clusters
+plot(1:15, wss, type = "b", 
+     xlab = "Number of Clusters", 
+     ylab = "Within groups sum of squares")
+
+# Set k equal to the number of clusters corresponding to the elbow location
+k <- 2
+
+# Practical matters: working with real data
+# Initialize total within sum of squares error: wss
+wss <- 0
+
+# Look over 1 to 15 possible clusters
+for (i in 1:15) {
+  # Fit the model: km.out
+  km.out <- kmeans(pokemon, centers = i, nstart = 20, iter.max = 50)
+  # Save the within cluster sum of squares
+  wss[i] <- km.out$tot.withinss
+}
+
+# Produce a scree plot
+plot(1:15, wss, type = "b", 
+     xlab = "Number of Clusters", 
+     ylab = "Within groups sum of squares")
+
+# Select number of clusters
+k <- 2
+
+# Build model with k clusters: km.out
+km.out <- kmeans(pokemon, centers = 2, nstart = 20, iter.max = 50)
+
+# View the resulting model
+km.out
+
+# Plot of Defense vs. Speed by cluster membership
+plot(pokemon[, c("Defense", "Speed")],
+     col = km.out$cluster,
+     main = paste("k-means clustering of Pokemon with", k, "clusters"),
+     xlab = "Defense", ylab = "Speed")
