6128052e6259c9d3a54434174cc7aeb84d44202f
Akshaykumar <cruseakshay@gmail.com>
2018-01-01 22:38:26 +0530

    added preprocessing

--- Text Mining/case_study.R
+++ Text Mining/case_study.R
@@ -17,3 +17,21 @@ goog_pros <- goog$pros
 # Create goog_cons
 goog_cons <- goog$cons
 
+# step 3: Text organization
+# Alter amzn_pros
+amzn_pros <- qdap_clean(amzn_pros)
+
+# Alter amzn_cons
+amzn_cons <- qdap_clean(amzn_cons)
+
+# Create az_p_corp 
+az_p_corp <- VCorpus(VectorSource(amzn_pros))
+
+# Create az_c_corp
+az_c_corp <- VCorpus(VectorSource(amzn_cons))
+
+# Create amzn_pros_corp
+amzn_pros_corp <- tm_clean(az_p_corp)
+
+# Create amzn_cons_corp
+amzn_cons_corp <- tm_clean(az_c_corp)
