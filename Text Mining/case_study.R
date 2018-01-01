71f9d5066d7a3f568cef88e20eee20e626b8c893
Akshaykumar <cruseakshay@gmail.com>
2018-01-01 22:30:00 +0530

    added EDA

---
+++ Text Mining/case_study.R
@@ -0,0 +1,19 @@
+# acquire dataset from url : 
+# Print the structure of amzn
+str(amzn)
+
+# Create amzn_pros
+amzn_pros <- amzn$pros
+
+# Create amzn_cons
+amzn_cons <- amzn$cons
+
+# Print the structure of goog
+str(goog)
+
+# Create goog_pros
+goog_pros <- goog$pros
+
+# Create goog_cons
+goog_cons <- goog$cons
+
