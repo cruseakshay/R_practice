836fe530e2eeb9b0adca69dcf17d3340a25a899f
Akshaykumar <cruseakshay@gmail.com>
2018-01-01 20:59:03 +0530

    added word association

--- Text Mining/tm skills.R
+++ Text Mining/tm skills.R
@@ -29,3 +29,47 @@ tdm1
 
 # Print tdm2
 tdm2
+
+# a text based dendrogram
+# note : A peculiarity of TDM and DTM objects is that you have to convert them first to matrices (with as.matrix()),
+# then to data frames (with as.data.frame()), before using them with the dist() function.
+
+# Create tweets_tdm2
+tweets_tdm2 <- removeSparseTerms(tweets_tdm, sparse = 0.975)
+
+# Create tdm_m
+tdm_m <- as.matrix(tweets_tdm2)
+
+# Create tdm_df
+tdm_df <- as.data.frame(tdm_m)
+
+# Create tweets_dist
+tweets_dist <- dist(tdm_df)
+
+# Create hc
+hc <- hclust(tweets_dist)
+
+# Plot the dendrogram
+plot(hc)
+
+# Dendrogram aesthetics
+# Load dendextend
+library(dendextend)
+
+# Create hc
+hc <- hclust(tweets_dist)
+
+# Create hcd
+hcd <- as.dendrogram(hc)
+
+# Print the labels in hcd
+labels(hcd)
+
+# Change the branch color to red for "marvin" and "gaye"
+branches_attr_by_labels(hcd, c("marvin", "gaye"), color = "red")
+
+# Plot hcd
+plot(hcd, main = "Better Dendrogram")
+
+# Add cluster rectangles 
+rect.dendrogram(hcd,k = 2, border = "grey50")
